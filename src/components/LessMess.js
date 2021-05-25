2ce448bdbadad3e04059db6fc4a2a6e1b30a09dc
zara <zamanian.zahra@gmail.com>
2021-05-26 00:46:43 +0430

    Add initial codes

---
+++ src/components/LessMess.js
@@ -0,0 +1,165 @@
+import React, { useState } from 'react';
+
+export const LessMess = (props) => {
+  const [itemForm, SetItemForm] = useState({
+    item: '',
+    material: '',
+    color: '',
+    size: '',
+    quality: '',
+    goodFor: '',
+    date: 1900,
+    likedItBecause: '',
+    more: '',
+  });
+
+  const change = (e) => {
+    SetItemForm({
+      ...itemForm,
+      [e.target.name]:
+        e.target.type === 'number' ? Number(e.target.value) : e.target.value,
+    });
+  };
+
+  console.log(itemForm);
+
+  return (
+    <>
+      <header>
+        <h1>LessMess!</h1>
+      </header>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Item</th>
+            <th>Material</th>
+            <th>Color</th>
+            <th>Size</th>
+            <th>Quality</th>
+            <th>Good for</th>
+            <th>Date</th>
+            <th>Liked it because</th>
+            <th>More</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {props.items.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.item}</td>
+              <td>{item.material}</td>
+              <td>{item.color}</td>
+              <td>{item.size}</td>
+              <td>{item.quality}</td>
+              <td>{item.goodFor}</td>
+              <td>{item.date}</td>
+              <td>{item.likedItBecause}</td>
+              <td>{item.more}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <form>
+        <div>
+          <label htmlFor="item-input">Item</label>
+          <input
+            type="text"
+            id="item-input"
+            value={itemForm.item}
+            onChange={change}
+            name="item"
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="material-input">Material</label>
+          <input
+            type="text"
+            id="material-input"
+            value={itemForm.material}
+            onChange={change}
+            name="material"
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="color-input">Color</label>
+          <input
+            type="text"
+            id="color-input"
+            value={itemForm.color}
+            onChange={change}
+            name="color"
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="size-input">Size</label>
+          <input
+            type="text"
+            id="size-input"
+            value={itemForm.size}
+            onChange={change}
+            name="size"
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="quality-select">Quality</label>
+          <select
+            id="quality-select"
+            value={itemForm.quality}
+            onChange={change}
+            name="quality"
+          >
+            <option>never used</option>
+            <option>perfect</option>
+            <option>good</option>
+            <option>almost good</option>
+            <option>bad</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="goodFor-input">Good for</label>
+          <input
+            type="text"
+            id="goodFor-input"
+            value={itemForm.goodFor}
+            onChange={change}
+            name="goodFor"
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="date-input">Date</label>
+          <input
+            type="number"
+            id="date-input"
+            value={itemForm.date}
+            onChange={change}
+            name="date"
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="likedItBecause-input">Liked it because</label>
+          <input
+            type="text"
+            id="likedItBecause-input"
+            value={itemForm.likedItBecause}
+            onChange={change}
+            name="likedItBecause"
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="more-input">More</label>
+          <input
+            type="text"
+            id="more-input"
+            value={itemForm.more}
+            onChange={change}
+            name="more"
+          ></input>
+        </div>
+      </form>
+    </>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//
+
+//zara
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { LessMess } from './components/LessMess';
+
+const itemsList = [
+  {
+    id: 1,
+    item: 'shirt',
+    material: 'cotton',
+    color: 'dark red',
+    size: '40',
+    quality: 'never used',
+    goodFor: 'women',
+    date: 2018,
+    likedItBecause: 'it is cool',
+    more: 'take care of it!',
+  },
+  {
+    id: 2,
+    item: 'mug',
+    material: 'pottery',
+    color: 'white',
+    size: '9cm*13cm',
+    quality: 'almost new',
+    goodFor: 'hot drinks',
+    date: 2015,
+    likedItBecause: 'it is funny',
+    more: 'nothing',
+  },
+];
+
+ReactDOM.render(
+  // React.createElement(LessMess),
+  <LessMess items={itemsList} />,
+  document.getElementById('root')
+);
