ed91df0aa4f52b5803f77f050956589e27abef89
zaaraa-z <zamanian.zahra@gmail.com>
2021-10-25 22:26:48 +0330

    improve performance using memo

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 
-export const AppHeader = ({appHeaderText, appSlogan}) => {
-    return <header>
-    <img src="../imgs/logo.svg" alt="logo"></img>
-    <h1>{appHeaderText}</h1>
-    <p>{appSlogan}</p>
-  </header>
-}
+export const AppHeader = memo(({ appHeaderText, appSlogan }) => {
+  return (
+    <header>
+      <img src="../imgs/logo.svg" alt="logo"></img>
+      <h1>{appHeaderText}</h1>
+      <p>{appSlogan}</p>
+    </header>
+  );
+});
 
 //defining the default values of params
 AppHeader.defaultProps = {
-    appHeaderText: "App Header",
-    appSlogan: "App Slogan"
-}
+  appHeaderText: 'App Header',
+  appSlogan: 'App Slogan',
+};
 
 //defining the types of params
 AppHeader.propTypes = {
-    appHeaderText: PropTypes.string,
-    appSlogan: PropTypes.string
-}
+  appHeaderText: PropTypes.string,
+  appSlogan: PropTypes.string,
+};
