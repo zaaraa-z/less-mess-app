c6b5146c05afaf56f80a2a7a5a2664ad8ddb2927
zaaraa-z <zamanian.zahra@gmail.com>
2021-10-28 18:27:24 +0330

    add a sample of class component to the app

--- src/components/EditTableRow.js
+++ src/components/EditTableRow.js
@@ -1,127 +1,140 @@
-import React from 'react';
+import React, { Component, createRef } from 'react';
 import PropTypes from 'prop-types';
 import { itemPropType } from '../propTypes/itemsPropType';
-import { useForm } from '../hooks/useForm';
-import { useDefaultInputFocus } from '../hooks/useDefaultInputFocus';
 
-export const EditTableRow = ({
-  item,
-  onSaveItem,
-  onCancelItem: cancelItem,
-}) => {
-  const [itemForm, change] = useForm({ ...item });
+export class EditTableRow extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...props.item,
+    };
 
-  const defaultFocusedInputRef = useDefaultInputFocus();
+    this.defaultFocusedInputRef = createRef();
+  }
 
-  const saveItem = () => {
-    onSaveItem({
-      ...itemForm,
-      id: item.id,
+  componentDidMount() {
+    if (this.defaultFocusedInputRef.current) {
+      this.defaultFocusedInputRef.current.focus();
+    }
+  }
+
+  change({ target: { name, type, value } }) {
+    this.SetState({
+      [name]: type === 'number' ? Number(value) : value,
     });
-  };
+  }
 
-  return (
-    <tr>
-      <td>{item.id}</td>
-      <td>
-        <input
-          id="edit-item-input"
-          type="text"
-          name="item"
-          value={itemForm.item}
-          onChange={change}
-          ref={defaultFocusedInputRef}
-        />
-      </td>
-      <td>
-        <input
-          id="edit-material-input"
-          type="text"
-          name="material"
-          value={itemForm.material}
-          onChange={change}
-        />
-      </td>
-      <td>
-        <input
-          id="edit-color-input"
-          type="text"
-          name="color"
-          value={itemForm.color}
-          onChange={change}
-        />
-      </td>
-      <td>
-        <input
-          id="edit-size-input"
-          type="text"
-          name="size"
-          value={itemForm.size}
-          onChange={change}
-        />
-      </td>
-      <td>
-        <select
-          value={itemForm.quality}
-          onChange={change}
-          name="quality"
-          id="edit-quality-select"
-        >
-          <option>never used</option>
-          <option>perfect</option>
-          <option>good</option>
-          <option>almost good</option>
-          <option>bad</option>
-        </select>
-      </td>
-      <td>
-        <input
-          id="edit-goodFor-input"
-          type="text"
-          name="goodFor"
-          value={itemForm.goodFor}
-          onChange={change}
-        />
-      </td>
-      <td>
-        <input
-          id="edit-date-input"
-          type="number"
-          name="date"
-          value={itemForm.date}
-          onChange={change}
-        />
-      </td>
-      <td>
-        <input
-          id="edit-likeItBecause-input"
-          type="text"
-          name="likeItBecause"
-          value={itemForm.likeItBecause}
-          onChange={change}
-        />
-      </td>
-      <td>
-        <input
-          id="edit-more-input"
-          type="text"
-          name="more"
-          value={itemForm.more}
-          onChange={change}
-        />
-      </td>
+  saveItem() {
+    this.props.onSaveItem({
+      ...this.state,
+      id: this.props.item.id,
+    });
+  }
 
-      <td>
-        <button type="button" onClick={cancelItem}>
-          Cancel
-        </button>
-        <button type="button" onClick={saveItem}>
-          Save
-        </button>
-      </td>
-    </tr>
-  );
-};
+  render() {
+    return (
+      <tr>
+        <td>{this.props.item.id}</td>
+        <td>
+          <input
+            id="edit-item-input"
+            type="text"
+            name="item"
+            value={this.state.item}
+            onChange={this.change}
+            ref={this.defaultFocusedInputRef}
+          />
+        </td>
+        <td>
+          <input
+            id="edit-material-input"
+            type="text"
+            name="material"
+            value={this.state.material}
+            onChange={this.change}
+          />
+        </td>
+        <td>
+          <input
+            id="edit-color-input"
+            type="text"
+            name="color"
+            value={this.state.color}
+            onChange={this.change}
+          />
+        </td>
+        <td>
+          <input
+            id="edit-size-input"
+            type="text"
+            name="size"
+            value={this.state.size}
+            onChange={this.change}
+          />
+        </td>
+        <td>
+          <select
+            value={this.state.quality}
+            onChange={this.change}
+            name="quality"
+            id="edit-quality-select"
+          >
+            <option>never used</option>
+            <option>perfect</option>
+            <option>good</option>
+            <option>almost good</option>
+            <option>bad</option>
+          </select>
+        </td>
+        <td>
+          <input
+            id="edit-goodFor-input"
+            type="text"
+            name="goodFor"
+            value={this.state.goodFor}
+            onChange={this.change}
+          />
+        </td>
+        <td>
+          <input
+            id="edit-date-input"
+            type="number"
+            name="date"
+            value={this.state.date}
+            onChange={this.change}
+          />
+        </td>
+        <td>
+          <input
+            id="edit-likeItBecause-input"
+            type="text"
+            name="likeItBecause"
+            value={this.state.likeItBecause}
+            onChange={this.change}
+          />
+        </td>
+        <td>
+          <input
+            id="edit-more-input"
+            type="text"
+            name="more"
+            value={this.state.more}
+            onChange={this.change}
+          />
+        </td>
+
+        <td>
+          <button type="button" onClick={this.props.OncancelItem}>
+            Cancel
+          </button>
+          <button type="button" onClick={this.saveItem}>
+            Save
+          </button>
+        </td>
+      </tr>
+    );
+  }
+}
 
 EditTableRow.propTypes = {
   item: itemPropType.isRequired,
---
+++ src/components/EditTableRowFunctional.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { itemPropType } from '../propTypes/itemsPropType';
+import { useForm } from '../hooks/useForm';
+import { useDefaultInputFocus } from '../hooks/useDefaultInputFocus';
+
+export const EditTableRow = ({
+  item,
+  onSaveItem,
+  onCancelItem: cancelItem,
+}) => {
+  const [itemForm, change] = useForm({ ...item });
+
+  const defaultFocusedInputRef = useDefaultInputFocus();
+
+  const saveItem = () => {
+    onSaveItem({
+      ...itemForm,
+      id: item.id,
+    });
+  };
+
+  return (
+    <tr>
+      <td>{item.id}</td>
+      <td>
+        <input
+          id="edit-item-input"
+          type="text"
+          name="item"
+          value={itemForm.item}
+          onChange={change}
+          ref={defaultFocusedInputRef}
+        />
+      </td>
+      <td>
+        <input
+          id="edit-material-input"
+          type="text"
+          name="material"
+          value={itemForm.material}
+          onChange={change}
+        />
+      </td>
+      <td>
+        <input
+          id="edit-color-input"
+          type="text"
+          name="color"
+          value={itemForm.color}
+          onChange={change}
+        />
+      </td>
+      <td>
+        <input
+          id="edit-size-input"
+          type="text"
+          name="size"
+          value={itemForm.size}
+          onChange={change}
+        />
+      </td>
+      <td>
+        <select
+          value={itemForm.quality}
+          onChange={change}
+          name="quality"
+          id="edit-quality-select"
+        >
+          <option>never used</option>
+          <option>perfect</option>
+          <option>good</option>
+          <option>almost good</option>
+          <option>bad</option>
+        </select>
+      </td>
+      <td>
+        <input
+          id="edit-goodFor-input"
+          type="text"
+          name="goodFor"
+          value={itemForm.goodFor}
+          onChange={change}
+        />
+      </td>
+      <td>
+        <input
+          id="edit-date-input"
+          type="number"
+          name="date"
+          value={itemForm.date}
+          onChange={change}
+        />
+      </td>
+      <td>
+        <input
+          id="edit-likeItBecause-input"
+          type="text"
+          name="likeItBecause"
+          value={itemForm.likeItBecause}
+          onChange={change}
+        />
+      </td>
+      <td>
+        <input
+          id="edit-more-input"
+          type="text"
+          name="more"
+          value={itemForm.more}
+          onChange={change}
+        />
+      </td>
+
+      <td>
+        <button type="button" onClick={cancelItem}>
+          Cancel
+        </button>
+        <button type="button" onClick={saveItem}>
+          Save
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+EditTableRow.propTypes = {
+  item: itemPropType.isRequired,
+  onSaveItem: PropTypes.func.isRequired,
+  onCancelItem: PropTypes.func.isRequired,
+};
